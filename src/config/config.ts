c0099da0619780470db1c058bf336526ef3195b5
Giannis Kokolis <john_97_kok@hotmail.com>
2025-07-02 18:45:33 +0300

    Externalize configuration

--- src/config/config.ts
+++ src/config/config.ts
@@ -6,7 +6,23 @@ dotenv.config({
 });
 
 export const config = {
-  port: process.env.PORT || 3000,
+  port: Number(process.env.PORT) || 3000,
   mongoUri: process.env.MONGO_URI || '',
   nodeEnv: process.env.NODE_ENV || 'development',
+  sessionSecret: process.env.SESSION_SECRET || 'some-secret',
+  keycloak: {
+    realm: process.env.KEYCLOAK_REALM || 'vre',
+    authServerUrl: process.env.KEYCLOAK_URL || 'http://localhost:8080/auth',
+    sslRequired: process.env.KEYCLOAK_SSL_REQUIRED || 'external',
+    resource: process.env.KEYCLOAK_RESOURCE || 'lab_repository',
+    confidentialPort: Number(process.env.KEYCLOAK_CONFIDENTIAL_PORT) || 0,
+  },
+  swaggerHost: process.env.SWAGGER_HOST || `localhost:${process.env.PORT || 3000}`,
+  jwt: {
+    jwksUri:
+      process.env.JWT_CERTS_URL ||
+      'http://localhost:8080/realms/vre/protocol/openid-connect/certs',
+    issuer: process.env.JWT_ISSUER || 'http://localhost:8080/realms/vre',
+    audience: process.env.JWT_AUDIENCE || 'nextjs-frontend',
+  },
 };
